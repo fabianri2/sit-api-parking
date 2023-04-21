726d432dd50662dc97cadf887551376aa58cde4a
fabianri2 <erodriguezi@upao.edu.pe>
2023-04-21 02:14:02 -0500

    feat: Crear la entidad Car

--- api.parcking/src/main/java/com/example/apiparcking/entities/Car.java
+++ api.parcking/src/main/java/com/example/apiparcking/entities/Car.java
@@ -1,4 +1,38 @@
 package com.example.apiparcking.entities;
 
+import java.time.LocalDateTime;
+
 public class Car {
+    private String licensePLate;
+    private String color;
+    private LocalDateTime entryTime;
+
+    public Car(String licensePLate, String color) {
+        this.licensePLate = licensePLate;
+        this.color = color;
+    }
+
+    public String getLicensePLate() {
+        return licensePLate;
+    }
+
+    public void setLicensePLate(String licensePLate) {
+        this.licensePLate = licensePLate;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public LocalDateTime getEntryTime() {
+        return entryTime;
+    }
+
+    public void setEntryTime(LocalDateTime entryTime) {
+        this.entryTime = entryTime;
+    }
 }
