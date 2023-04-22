80f8ff2a0d0830d1baabb5c73383b901cd5aab32
fabianri2 <erodriguezi@upao.edu.pe>
2023-04-22 00:27:06 -0500

    feat: Crear la clase ParkingService. Implemento los metodos getAllCars y addCar

--- api.parcking/src/main/java/com/example/apiparcking/controller/Controller.java
+++ api.parcking/src/main/java/com/example/apiparcking/controller/Controller.java
@@ -1,4 +1,33 @@
 package com.example.apiparcking.controller;
 
+import com.example.apiparcking.entities.Car;
+import com.example.apiparcking.services.ParkingService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RequestMapping("/parking")
+@RestController
 public class Controller {
+
+    private final ParkingService parkingService;
+
+    public Controller(ParkingService parkingService){
+        this.parkingService = parkingService;
+    }
+    @GetMapping("/cars")
+    public ResponseEntity<List<Car>> getAllCars(){
+        return ResponseEntity.ok(this.parkingService.getAllCars();
+    }
+    @PostMapping ("/cars")
+    public ResponseEntity<Car> addCar(Car car){
+        this.parkingService.addCar(car);
+        return new ResponseEntity<>(car, HttpStatus.CREATED);
+    }
 }
--- api.parcking/src/main/java/com/example/apiparcking/services/ParkingService.java
+++ api.parcking/src/main/java/com/example/apiparcking/services/ParkingService.java
@@ -1,23 +1,29 @@
 package com.example.apiparcking.services;
 
 import com.example.apiparcking.entities.Car;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Service
 public class ParkingService {
 
     private List<Car> parkingLot;
 
     public ParkingService(){
+
         this.parkingLot = new ArrayList<>();
     }
 
     public List<Car> getAllCars() {
+
         return this.parkingLot;
     }
 
-    public void addCar(Car car){
+    public ResponseEntity<Car> addCar(Car car){
         this.parkingLot.add(car);
+        return null;
     }
 }
